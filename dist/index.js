ebde514f95b85718cf784b7cff5a3e5b4a57da05
Bartosz Stopa <stopa323@gmail.com>
2020-06-28 12:37:24 +0200

    Move files to src

---
+++ dist/index.js
@@ -0,0 +1,93 @@
+module.exports =
+/******/ (function(modules, runtime) { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		var threw = true;
+/******/ 		try {
+/******/ 			modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/ 			threw = false;
+/******/ 		} finally {
+/******/ 			if(threw) delete installedModules[moduleId];
+/******/ 		}
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	__webpack_require__.ab = __dirname + "/";
+/******/
+/******/ 	// the startup function
+/******/ 	function startup() {
+/******/ 		// Load entry module and return exports
+/******/ 		return __webpack_require__(497);
+/******/ 	};
+/******/
+/******/ 	// run startup
+/******/ 	return startup();
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ 292:
+/***/ (function(module) {
+
+module.exports = eval("require")("@slack/web-api");
+
+
+/***/ }),
+
+/***/ 497:
+/***/ (function(__unusedmodule, __unusedexports, __webpack_require__) {
+
+
+const { WebClient } = __webpack_require__(292);
+
+const core = __webpack_require__(968);
+
+const token = core.getInput('slackBotToken');
+const channel = core.getInput('slackChannel');
+
+const client = new WebClient(token);
+
+(async () => {
+  const result = await client.chat.postMessage({
+    channel: channel,
+    text: 'Build started',
+  });
+
+  console.log(`Posted message ${result.ts} to ${channel}`);
+})();
+
+
+/***/ }),
+
+/***/ 968:
+/***/ (function(module) {
+
+module.exports = eval("require")("@actions/core");
+
+
+/***/ })
+
+/******/ });
