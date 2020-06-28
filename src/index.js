7518507b238baa723255dbedff3179233213be4e
Bartosz Stopa <stopa323@gmail.com>
2020-06-28 15:57:40 +0200

    Test stage changing through core.getState

--- src/index.js
+++ src/index.js
@@ -7,12 +7,33 @@ const channel = core.getInput('slackChannel');
 
 const client = new WebClient(token);
 
-(async () => {
-  var stage = core.getState("stage");
+async function post() {
+  console.log(`post function`);
+}
+
+async function main() {
+  console.log(`main function`);
+}
+
+async function run() {
+  try {
+      var stage = core.getState("actionStage");
+      if ("post" == stage) {
+        await post();
+      }
+      else {
+        await main();
+        core.saveState("actionStage", "post");
+      }
+  }
+  catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
+
   // const result = await client.chat.postMessage({
   //   channel: channel,
   //   text: 'Build in progress',
   // });
-
-  console.log(`Posted message ${stage}`);
-})();
