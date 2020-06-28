b7cbe8475b18815655aa0b69828185f9f2934cb7
Bartosz Stopa <stopa323@gmail.com>
2020-06-28 10:11:27 +0200

    Test posting to Slack

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+const { WebClient } = require('@slack/web-api');
 
-try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
-}
+const core = require('@actions/core');
+
+const token = core.getInput('slackBotToken');
+const channel = core.getInput('slackChannel');
+
+const client = new WebClient(token);
+
+(async () => {
+  const result = await client.chat.postMessage({
+    channel: channel,
+    text: 'Hello world!',
+  });
+
+  console.log(`Successfully send message ${result.ts} in conversation`);
+})();
